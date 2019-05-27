9b6bea80a773f0e9966a1514554d2c84e7234b18
srikanth-paladugula <srikanth.paladugula@outlook.com>
2019-05-27 12:52:40 +0530

    Updated the gitignore, to include generated folder.

---
+++ Controls/GroupedOptionsetControl/GroupedOptionsetControl/generated/ManifestTypes.d.ts
@@ -0,0 +1,12 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    optionsetFieldControl: ComponentFramework.PropertyTypes.OptionSetProperty;
+    groupingOptions?: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    optionsetFieldControl?: number;
+}
